65d0c21b7d6e4bf13a9eb41ea7a02abc5bcfc820
micha1bogdanowicz <polrider@protonmail.com>
2017-01-05 06:46:28 +0100

    Zabawa z html na stronie

--- django_blog/wsgi.py
+++ django_blog/wsgi.py
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 
 application = get_wsgi_application()
+#wsgi z pythonanywhere
+#import os
+#import sys
+#
+#path = '/home/polrider/Django-blog'
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'django_blog.settings'
+#
+#from django.core.wsgi import get_wsgi_application
+#from whitenoise.django import DjangoWhiteNoise
+#application = DjangoWhiteNoise(get_wsgi_application())
